e1fc0a2c8f2133e54a4b95b999027b7b97770adb
mike <freemike172003@yahoo.com>
2013-04-18 00:05:54 -0700

    sign up codes  and jsp files, not working so far.

---
+++ src/com/beans/Registration.java
@@ -0,0 +1,70 @@
+package com.beans;
+
+/*import javax.validation.constraints.Size;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;*/
+
+
+
+public class Registration {
+	
+	
+	private String fname;
+	
+	private String lname;
+    //@NotEmpty
+    //@Size(min = 4, max = 20)
+    private String password;
+   // @NotEmpty
+    private String confirmPassword;
+   // @NotEmpty
+    //@Email
+    private String email;
+
+    
+    public void setfname(String fname) {
+            this.fname = fname;
+    }
+
+    public String getfname() {
+            return fname;
+    }
+
+    
+    public void setlname(String lname) {
+        this.lname = lname;
+}
+
+public String getlname() {
+        return lname;
+}
+    
+    
+    
+    public void setPassword(String password) {
+            this.password = password;
+    }
+
+    public String getPassword() {
+            return password;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+            this.confirmPassword = confirmPassword;
+    }
+
+    public String getConfirmPassword() {
+            return confirmPassword;
+    }
+
+    public void setEmail(String email) {
+            this.email = email;
+    }
+
+    public String getEmail() {
+            return email;
+    }
+
+	
+
+}
---
+++ src/com/beans/RegistrationValidation.java
@@ -0,0 +1,44 @@
+package com.beans;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import com.beans.Registration;
+
+@Component("registrationValidator")
+public class RegistrationValidation {
+
+	public boolean supports(Class<?> klass) {
+		return Registration.class.isAssignableFrom(klass);
+	}
+
+	public void validate(Object target, Errors errors) {
+		Registration registration = (Registration) target;
+
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "fname",
+				"NotEmpty.registration.fname", "User Name must not be Empty.");
+		String fname = registration.getfname();
+
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lname",
+				"NotEmpty.registration.lname", "User Name must not be Empty.");
+		String lname = registration.getlname();
+
+		if ((fname.length()) > 50) {
+			errors.rejectValue("fname", "lengthOfUser.registration.fname",
+					"User Name must not more than 50 characters.");
+		}
+
+		if ((lname.length()) > 50) {
+			errors.rejectValue("lname", "lengthOfUser.registration.lname",
+					"User Name must not more than 50 characters.");
+		}
+
+		if (!(registration.getPassword()).equals(registration
+				.getConfirmPassword())) {
+			errors.rejectValue("password",
+					"matchingPassword.registration.password",
+					"Password and Confirm Password Not match.");
+		}
+	}
+
+}
