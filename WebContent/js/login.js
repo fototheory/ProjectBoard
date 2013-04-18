41adca1d7edcc805aa330e5ea7637aeb77cd5e25
Darryl Thierry <fototheory@gmail.com>
2013-04-18 13:43:28 +0900

    Updated login.js
    Update login.js with Yumiko's code to fix the problem with the login
    drop down button.

--- WebContent/js/login.js
+++ WebContent/js/login.js
@@ -6,16 +6,16 @@ $(function() {
     var form = $('#loginForm');
     button.removeAttr('href');
     button.mouseup(function(login) {
-        box.toggle();
-        button.toggleClass('active');
+    	if(!($(login.target).parent('#loginButton').length > 0)) {
+    		button.removeClass('active');
+            box.hide();
+    	}
+    	else {
+    		box.toggle();
+    		button.toggleClass('active');
+    	}
     });
     form.mouseup(function() { 
         return false;
-    });
-    $(this).mouseup(function(login) {
-        if(!($(login.target).parent('#loginButton').length > 0)) {
-            button.removeClass('active');
-            box.hide();
-        }
     });
 });
