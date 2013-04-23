d6c9307f11ba66794fc6aaa1e3e879f2f345cd97
Darryl Thierry <fototheory@gmail.com>
2013-04-24 01:46:22 +0900

    Registration Page
    For the most part it is done.  There are still a couple of things that
    come to mind that need to be done.
    
    1. Need to check if password conforms to password requirements (i.e. >4
    <20, numbers, etc.)
    2. Need to check to see if an email is in a valid format (i.e.
    address@home.com)
    3. Registration success form should be formatted for our current layout
    and maybe add a ok button or something like that.

--- src/com/controller/RegistrationValidation.java
+++ src/com/controller/RegistrationValidation.java
@@ -3,19 +3,21 @@ package com.controller;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
-
+import org.springframework.validation.Validator;
 import com.form.Registration;
 
 @Component("registrationValidator")
-public class RegistrationValidation {
+public class RegistrationValidation implements Validator{
 
 	public boolean supports(Class<?> klass) {
 		return Registration.class.isAssignableFrom(klass);
 	}
 
+	@Override
 	public void validate(Object target, Errors errors) {
 		Registration registration = (Registration) target;
 
+		//Check first name & last name fields
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "fname",
 				"NotEmpty.registration.fname", "First Name must not be Empty.");
 		String fname = registration.getFname();
@@ -34,11 +36,12 @@ public class RegistrationValidation {
 					"User Name must not more than 50 characters.");
 		}
 
-		if (!(registration.getPassword()).equals(registration
-				.getConfirmPassword())) {
+		//Check password fields
+//Still need to check whether the password conforms to our password rules (i.e. >4 <20 characters)
+		if (!(registration.getPassword()).equals(registration.getConfirmPassword())) {
 			errors.rejectValue("password",
 					"matchingPassword.registration.password",
-					"Password and Confirm Password Not match.");
+					"Password and Confirm Password do not match.");
 		}
 
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password",
@@ -47,12 +50,18 @@ public class RegistrationValidation {
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "confirmPassword",
 				"NotEmpty.registration.confirmPassword", "Password must not be Empty.");
 		
+		//Check email field
+//Still need to check whether the email is in a valid format
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email",
 				"NotEmpty.registration.email", "Email must not be Empty.");
+		
+		//Check discipline and roles to make sure one is selected
+		if (registration.getRole() == 0) {
+			errors.rejectValue("role","NotEmpty.registration.role","A Role must be selected.");	
+		}
 
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "discipline", "NotEmpty.registration.discipline", "A discipline is required");
-
-		//ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userrole", "A role is required");
+		if (registration.getDiscipline() == 0) {
+			errors.rejectValue("discipline","NotEmpty.registration.discipline","A Discipline must be selected.");
+		}
 	}
-
 }
