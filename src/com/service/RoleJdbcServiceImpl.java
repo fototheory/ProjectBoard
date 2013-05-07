b79b0e2cb773798c8cca5e02a0f115ccfb99dae6
Yumiko Iwai <iwai_yumiko@yahoo.com>
2013-05-07 13:03:38 -0700

    Missing Darryl's Branch Commit
    RoleJdbcServiceImpl.java

--- src/com/service/RoleJdbcServiceImpl.java
+++ src/com/service/RoleJdbcServiceImpl.java
@@ -1,6 +1,5 @@
 package com.service;
 
-import com.beans.Profile;
 import com.beans.Role;
 import com.dao.RoleJdbcDaoImpl;
 import java.util.List;
@@ -24,33 +23,65 @@ public class RoleJdbcServiceImpl implements SpringJdbcService<Role> {
 	public void setSpringJdbcDao(RoleJdbcDaoImpl springJdbcDao) {
 		this.roleJdbcDao = springJdbcDao;
 	}
+	
 	/**
-	 * generic select by id
-	 * @param id role_id of role table
+	 * Select a role by its id
+	 * @param id a role id
 	 * @return returns empty role object if a role_id doesn't have match in database, 
-	 * otherwise, returns a role in the list
+	 * otherwise, returns a role record
 	 */
 	@Override
 	public Role selectById(int id) {
 		return roleJdbcDao.selectById(id);
 	}
 	
+	/**
+	 * Select all roles
+	 * @return returns a list of roles 
+	 */
 	public List<Role> selectAllRoles() {
 		return roleJdbcDao.selectAllRoles();
 	}
+	
+	/**
+	 * Select all roles except the admin role
+	 * @return a list of all roles except the admin role
+	 */
+	public List<Role> selectAllRolesExceptAdmin() {
+		return roleJdbcDao.selectAllRolesExceptAdmin();
+	}
 
+	/**
+	 * Insert a role
+	 * @param role a Role object
+	 * @return int
+	 */
 	public int insert(Role role) {
 		return roleJdbcDao.insert(role);
 	}
 	
+	/**
+	 * Update a role with a role object
+	 * @param role a Role object
+	 * @return returns the number of records affected by the update
+	 */
 	public int update(Role role) {
 		return roleJdbcDao.update(role);
 	}
 	
+	/**
+	 * Delete a role by the role id
+	 * @param roleID the role's id number
+	 * @return returns the number of records deleted
+	 */
 	public int delete(int roleId) {
 		return roleJdbcDao.deleteById(roleId);
 	}
 	
+	/**
+	 * Get the last role's id number
+	 * @return returns the role_id of the last role in the table
+	 */
 	public int getLastId() {
 		return roleJdbcDao.getLastRoleId();
 	}
